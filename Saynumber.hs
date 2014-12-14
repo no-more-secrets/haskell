dcdf574dce11f999b9651c324bbf21db68c86929
David P. Sicilia <dpsicilia@gmail.com>
2014-12-14 00:10:02 -0500

    Final version of Saynumber

--- Saynumber.hs
+++ Saynumber.hs
@@ -1,31 +1,31 @@
 module Saynumber (sayNumber) where
 
 import Data.List (genericIndex)
+import Control.Monad (guard)
 import Utils
 
 --------------------------------------------------------------------------------
 -- Say an integer in English form
 
-hundreds = splitC ",one hundred,two hundred,three hundred,four hundred,five hundred,six hundred,seven hundred,eight hundred,nine hundred"
-oneTo9   = splitC ",one,two,three,four,five,six,seven,eight,nine"
-teens    = splitC "ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen"
-tens     = splitC "twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety"
+oneTo19  = (enumerate · splitC) ",one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,nineteen"
+hundreds = (enumerate · splitC) ",one hundred,two hundred,three hundred,four hundred,five hundred,six hundred,seven hundred,eight hundred,nine hundred"
+tens     = (enumerate · splitC) ",twenty,thirty,forty,fifty,sixty,seventy,eighty,ninety"
 ranks    = splitC ",thousand,million,billion,trillion,quadrillion"
 
 sayNumber :: (Integral a) => a -> String
 sayNumber n
-    | (n == 0) = "zero"
-    | (n <  0) = "negative " ++ sayNumber (-n)
-    | (n >  0) = join ", " · reverse · map joinStripPair · remove (null · fst) · (`zip` ranks') · map oneTo999 · map (`mod` 1000) · takeWhile (>0) · iterate (`div` 1000) $ n
+    | (n == 0)  = "zero"
+    | (n <  0)  = "negative " ++ sayNumber (-n)
+    | otherwise = join ", " · reverse · map joinStripPair · remove (null · fst) · flip zip ranks' · map (genericIndex oneTo999 · flip mod 1000) · takeWhile (>0) · iterate (`div` 1000) $ n
 
-        where ranks'   = ranks ++ (tail ranks') `zipSpace` (repeat · last $ ranks)
-              oneTo999 = genericIndex       [joinStripPair (x,y) | x <- hundreds, y <- oneTo99]
-              oneTo99  = oneTo9 ++ teens ++ [joinStripPair (x,y) | x <- tens,     y <- oneTo9]
+        where oneTo999 = [ joinStrip [x,y,z] | (x',x) <- hundreds, (y',y) <- tens, (z',z) <- oneTo19, (y'==0 || z'<10) ]
+              ranks'   = ranks ++ (tail ranks') `zipSpace` (repeat · last $ ranks)
 
 --------------------------------------------------------------------------------
 -- Tests for sayNumber
 
-runTests = and sayNumberTests
+runTests = putStrLn $ if and sayNumberTests then "Success" else "Failed"
+
 sayNumberTests = [sayNumber 0      == "zero",
                   sayNumber 1      == "one",
                   sayNumber 10     == "ten",
@@ -42,7 +42,10 @@ sayNumberTests = [sayNumber 0      == "zero",
                                       "three hundred forty three billion, four hundred thirty four million, "      ++
                                       "three hundred forty three thousand, four hundred thirty four",
                   sayNumber large2 == "one hundred forty nine billion quadrillion, one thousand quadrillion, " ++
-                                      "four hundred forty four quadrillion, five billion, seven hundred eighty million"]
+                                      "four hundred forty four quadrillion, five billion, seven hundred eighty million",
+                  sayNumber large3 == "thirty eight quadrillion, four hundred thirty eight trillion, " ++
+                                      "seven hundred eighty seven billion, five"]
 
                   where large1 = 2323434511343434343434      :: Integer
                         large2 = 149000001444000005780000000 :: Integer
+                        large3 = 38438787000000005           :: Integer
