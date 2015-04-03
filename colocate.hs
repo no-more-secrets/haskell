70d20f30538902e6653480fe845ccffc939f1f6b
David P. Sicilia <dpsicilia@gmail.com>
2015-04-03 17:53:52 -0400

    Tweaking colocate

--- colocate.hs
+++ colocate.hs
@@ -2,18 +2,15 @@ import Data.List (transpose)
 import Data.Maybe (fromMaybe)
 import Safe (maximumMay)
 import System.Environment (getArgs)
+import Utils
 
 --pad :: a -> Int -> [a] -> [a]
 pad e max xs = xs ++ replicate (max - length xs) e
 
 --addPadding :: a -> [[a]] -> [[a]]
-addPadding e xss = map (pad e . fromMaybe 0 . maximumMay . map length $ xss) xss
-
---zipText :: [[String]] -> [[String]]
-zipText = transpose . map (addPadding ' ') . addPadding ""
+addPadding e xss = map (pad e · fromMaybe 0 · maximumMay · map length $ xss) xss
 
 --colocate :: [String] -> String
-colocate = unlines . map concat . zipText . map lines
+colocate = unlines · map concat · transpose · map (addPadding ' ') · addPadding "" · map lines
 
---main :: IO ()
-main = getArgs >>= (sequence . map readFile) >>= (putStrLn . colocate)
+main = (putStrLn · colocate) ·· (sequence · map readFile) =<< getArgs
