94f549e3219e3b9d64183bf08c70765e290b71ed
David P. Sicilia <dpsicilia@gmail.com>
2015-04-03 21:27:54 -0400

    Further refine colocate

--- colocate.hs
+++ colocate.hs
@@ -1,16 +1,11 @@
 import Data.List (transpose)
-import Data.Maybe (fromMaybe)
-import Safe (maximumMay)
 import System.Environment (getArgs)
-import Utils
 
---pad :: a -> Int -> [a] -> [a]
-pad e max xs = xs ++ replicate (max - length xs) e
-
---addPadding :: a -> [[a]] -> [[a]]
-addPadding e xss = map (pad e · fromMaybe 0 · maximumMay · map length $ xss) xss
+--pad :: a -> [[a]] -> [[a]]
+pad x xs = [ys ++ replicate (max - length ys) x | ys <- xs]
+    where max = (maximum . map length) xs
 
 --colocate :: [String] -> String
-colocate = unlines · map concat · transpose · map (addPadding ' ') · addPadding "" · map lines
+colocate = unlines . map concat . transpose . map (pad ' ') . pad [] . map lines
 
-main = (putStrLn · colocate) ·· (sequence · map readFile) =<< getArgs
+main = (putStrLn . colocate) =<< mapM readFile =<< getArgs
