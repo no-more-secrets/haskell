99325bc3339a534934e9210635dcb43d97a3ccc7
David P. Sicilia <dpsicilia@gmail.com>
2015-04-22 18:15:42 -0400

    Unzip basically done

--- Unzip.hs
+++ Unzip.hs
@@ -1,39 +1,64 @@
+module Unzip (optimize) where
+
 import Data.Function (on)
-import Data.List (sort, groupBy)
+import Data.List (sort, groupBy, partition)
 import Prefix (commonPrefix, slashes, unslashes)
 import System.Environment (getArgs)
 import Utils (onLines, remove, equating)
 
+--------------------------------------------------------------------------------
+-- Interface
+
+-- Takes a number of processes and a String consisting of a list of files
+-- and folders in the format output by unzip -l.  It will return a String
+-- containing a subset of those files/folder (referred to as unzip targets)
+-- which will number less or equal to `processes` such that if we selectively
+-- unzip only those targets from the zip file then it will recover the entire
+-- file structure in the zip.  Note that each unzip target in the result,
+-- when extracted, will case a set of files/folders to be extracted and
+-- this set will not overlap with that of any of the other unzip targets.
+
+-- optimize :: Int -> String -> String
+optimize processes = onLines (map unslashes . optimize' . map slashes . sort)
+    where optimize' = uncurry (++) . partition (null . last) . distribute processes
+
+--------------------------------------------------------------------------------
+-- Implementation
+
+-- This is a single file/folder path split on the path separator
 type Path = [String]
 
-floorOne n | (n < 1)   = 1
-           | otherwise = n
-
---normalize :: (Fractional a) => [Int] -> [a]
-normalize xs = map (/sum xsF) xsF
-    where xsF = map fromIntegral xs
-
--- weightsToCounts :: Int -> [Int] -> [Int]
-scale count = map (floorOne . floor . (*fromIntegral count)) . normalize
-
--- toUnzipList :: Int -> [Path] -> [Path]
-toUnzipList count paths
+-- distribute :: Int -> [Path] -> [Path]
+distribute count paths
     | (null . tail) paths    = paths
     | (count < length items) = return (foldr1 commonPrefix paths)
     | otherwise              = recurse =<< (zip (scale count lengths) items)
     where
-        --recurse :: (Int, String, [Path]) -> [Path]
-        recurse (newCount, newPaths) = (map (prefix:) . toUnzipList newCount . map tail) newPaths
-            where prefix = (head . head) newPaths
-
         items   = groupBy (equating head) paths
         lengths = map length items
 
---filesToInstructions :: Int -> String -> String
-filesToInstructions processes = onLines (map unslashes . optimize . map slashes . sort)
-    where optimize = remove (null . last) . toUnzipList processes
+        --recurse :: (Int, String, [Path]) -> [Path]
+        recurse (newCount, newPaths) = (map (prefix:) . distribute newCount . map tail) newPaths
+            where prefix = (head . head) newPaths
 
-main = (interact . filesToInstructions . read . head) =<< getArgs
+        --scale :: Int -> [Int] -> [Int]
+        scale count = map (max 1 . floor . (*fromIntegral count)) . normalize
 
---mainTest = (putStrLn . show . spectrum) =<< readFile "files.txt"
---    where spectrum txt = take 400 . map (length . lines . (`filesToInstructions` txt)) $ [1..]
+--------------------------------------------------------------------------------
+-- Utilities
+
+normalize :: (Fractional a) => [Int] -> [a]
+normalize xs = map (/ fromIntegral (sum xs)) (map fromIntegral xs)
+
+--------------------------------------------------------------------------------
+-- Program to read lines from stdin
+
+main :: IO ()
+main = (interact . optimize . read . head) =<< getArgs
+
+--------------------------------------------------------------------------------
+-- For testing
+
+mainTest :: IO ()
+mainTest = (putStrLn . show . spectrum) =<< readFile "files.txt"
+    where spectrum txt = (take 200 . map (length . lines . (`optimize` txt))) [1..]
