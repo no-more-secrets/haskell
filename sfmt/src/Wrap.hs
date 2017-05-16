d5af6e2ba6669aa79345136929a518a4137fb25d
David P. Sicilia <dpsicilia@gmail.com>
2017-05-16 08:17:43 -0400

    Finish off Wrap.hs

--- sfmt/src/Wrap.hs
+++ sfmt/src/Wrap.hs
@@ -8,11 +8,6 @@ import Data.List (inits, splitAt)
 import Hyphen (dehyphenate, hyphenChunks)
 import Utils  (unfoldrList)
 
-wrapOne :: Int -> [String] -> ([String], [String])
-wrapOne n = (splitAt =<< cut)
-  where cut  = max 1 . length . takeWhile fits . tail . inits
-        fits = (<=n) . length . unwords . dehyphenate
-
 -- Take  number  of columns and input string representing a docu-
 -- ment and reformats the text so that it fits within the  speci-
 -- fied  number  of  columns.  If a given word is longer than the
@@ -20,5 +15,10 @@ wrapOne n = (splitAt =<< cut)
 -- this  line  will of course be longer than the target number of
 -- columns.
 wrapPara :: Int -> [String] -> [[String]]
-wrapPara n = map dehyphenate    . unfoldrList (wrapOne n)
-           . (hyphenChunks =<<) . dehyphenate
+wrapPara n = map dehyphenate . unfoldrList (wrapOne n)
+           . concatMap hyphenChunks . dehyphenate
+  where
+    wrapOne :: Int -> [String] -> ([String], [String])
+    wrapOne n = (splitAt =<< cut)
+      where cut  = max 1 . length . takeWhile fits . tail . inits
+            fits = (<=n) . length . unwords . dehyphenate
