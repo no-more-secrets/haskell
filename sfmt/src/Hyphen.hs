6995bcfbb6ef20059b32bdc4538d73d27597375e
David P. Sicilia <dpsicilia@gmail.com>
2017-08-08 07:56:08 -0400

    Some more cleanup of hyphenation/wrapping code

--- sfmt/src/Hyphen.hs
+++ sfmt/src/Hyphen.hs
@@ -1,34 +1,40 @@
 {-# LANGUAGE ViewPatterns #-}
 
-module Hyphen (hyphenate, dehyphenate, isHyp) where
+module Hyphen (hyphenate, dehyphenate, isFragment) where
 
 import Data.List (intersperse)
 import Utils     (chunks)
 
 import qualified Text.Hyphenation as H
 
-isHyp :: String -> Int
-isHyp []           = error "empty isHyp"
-isHyp ('-':[])     = 0
-isHyp ('-':'-':[]) = 0
-isHyp xs | last xs == '-' = 1
-         | otherwise      = 0
+-- The gold standard for determining whether  a word is a hyphen-
+-- ated fragment or not. Note that, e.g. "--", although  it  ends
+-- in a hyphen, is not considered a hyphenated fragment since the
+-- dashes are not intended to signify joining on the  next  word.
+isFragment :: String -> Bool
+isFragment ""            = False
+isFragment "-"           = False
+isFragment "--"          = False
+isFragment (last -> '-') = True
+isFragment _             = False
 
--- We can't do this with a  simple  map operation because we need
+-- We can't do this with a simple map operation because  we  need
 -- to join adjacent words after removing a hyphen.
 dehyphenate :: [String] -> [String]
-dehyphenate [ ] = [ ]
+dehyphenate []  = []
 dehyphenate [x] = [x]
 dehyphenate (x:y:rest)
-    | isHyp x == 0 = x:dehyphenate (y:rest)
-    | otherwise    = dehyphenate $ (init x++y):rest
+    | isFragment x = dehyphenate $ (init x++y):rest
+    | otherwise    = x:dehyphenate (y:rest)
 
+-- Make hyphenated fragments, but only if the  word  contains  no
+-- hyphens, since otherwise the it may not be possible to reverse
+-- the hyphenation and get back the same original word.
 hyphenate :: String -> [String]
-hyphenate s@(elem '-' -> True) = [s]
-hyphenate s = hyphens s
+hyphenate s | '-'`elem`s = [s]
+            | otherwise  = hyphenate' s
   where
     -- "documentation" ==> ["do-","cu-","ment-","ation"]
-    hyphens :: String -> [String]
-    hyphens = map concat . chunks 2 . intersperse "-" . hyphens'
-      where hyphens' = H.hyphenate H.english_US
-
+    hyphenate' :: String -> [String]
+    hyphenate' = map concat . chunks 2 . intersperse "-"
+               . H.hyphenate H.english_US
--- sfmt/src/Wrap.hs
+++ sfmt/src/Wrap.hs
@@ -1,23 +1,23 @@
 module Wrap (wrap, hyphenate, dehyphenate) where
 
-import Data.List (splitAt, transpose)
-import Hyphen    (hyphenate, dehyphenate, isHyp)
-import Utils     (unfoldrList)
+import Data.List  (splitAt, transpose)
+import Hyphen     (hyphenate, dehyphenate, isFragment)
+import Utils      (unfoldrList)
 
--- ["do-","cu-","ment-","ation","do-","cu-","ment-","ation"]
--- [    1,    1,      1,      0,    1,    1,      1,      0] -- has dash
--- [    2,    2,      4,      5,    2,    2,      4,      5] -- lengths no dash
--- [    0,    0,      0,      1,    0,    0,      0,      1] -- has no dash
--- [    0,    0,      0,      0,    1,    0,      0,      0] -- shifted
--- [    2,    2,      4,      5,    3,    2,      4,      5] -- lengths no dash shifted
--- [    2,    4,      8,     13,   16,   18,     22,     27] -- accum lengths no dash
--- [    3,    5,      9,     13,   17,   19,     23,     27] -- add
-longest :: Int -> [String] -> Int
-longest n xs = length . takeWhile (<=n)
-             $ zipWith (+) hyps . scanl1 (+) . map sum . transpose
-             $ [map length xs, 0:map (1-) hyps, map negate hyps]
+x -| y = zipWith (-) x y
+x +| y = zipWith (+) x y
+
+-- Takes  a  column size and list of hyphenated fragments and re-
+-- turns the maximum number of fragments that, after  dehypheniz-
+-- ing, would be able to  fit  within  the column size (including
+-- spaces between words).
+-- xs: ["do-","cu-","ment-","ation","do-","cu-","ment-","ation"]
+fit :: Int -> [String] -> Int
+fit n xs = length $ takeWhile (<=n) $ (hyp +|) $ scanl1 (+) $ zs
   where
-    hyps = map isHyp xs
+    -- length without hyphen + leading space if no hyphen before
+    zs  = map length xs -| hyp +| (0:map (1-) hyp)
+    hyp = map (fromEnum . isFragment) xs
 
 -- Word wrap a list of words to  fit  on  a line of size n (which
 -- includes spaces). Words may be hyphenated  to  attain  optimal
@@ -27,4 +27,4 @@ longest n xs = length . takeWhile (<=n)
 -- line will be > n.
 wrap :: Int -> [String] -> [[String]]
 wrap n = map dehyphenate . wrap' . (hyphenate =<<) . dehyphenate
-  where wrap' = unfoldrList (splitAt =<< max 1 . longest n)
+  where wrap' = unfoldrList (splitAt =<< max 1 . fit n)
